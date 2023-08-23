de7af7b2d75da047828c8adfccb4533c24330bec
javkhlantugs <jay.mudgil@gmail.com>
2023-08-23 14:58:10 -0400

    completed all projects

--- attention/t.py
+++ attention/t.py
@@ -37,8 +37,7 @@ def main():
         print(text.replace(tokenizer.mask_token, tokenizer.decode([token])))
 
     # Visualize attentions
-    visualize_attentions(inputs.tokens(), result.attentions)
-
+    print(visualize_attentions(inputs.tokens(), result.attentions))
 
 def get_mask_token_index(mask_token_id, inputs):
     """
@@ -46,9 +45,12 @@ def get_mask_token_index(mask_token_id, inputs):
     `None` if not present in the `inputs`.
     """
     
-
+    # for i, token in enumerate(inputs.input_ids[0]):
+    #     if token == mask_token_id:
+    #         return i
+    # return None
     return tf.where(inputs.input_ids[0] == mask_token_id)[0][0].numpy()
-
+    # return inputs.input_ids[0]
 
 
 def get_color_for_attention_score(attention_score):
@@ -70,61 +72,17 @@ def visualize_attentions(tokens, attentions):
     include both the layer number (starting count from 1) and head number
     (starting count from 1).
     """
-    
+
     for i, layer in enumerate(attentions):
-        beam = layer[0]
-        for j, head in enumerate(beam):
-            generate_diagram(i+1, j+1, tokens, head)
-    # print(len(attentions[0][0]))
+        return (i, layer)
 
-def generate_diagram(layer_number, head_number, tokens, attention_weights):
-    """
-    Generate a diagram representing the self-attention scores for a single
-    attention head. The diagram shows one row and column for each of the
-    `tokens`, and cells are shaded based on `attention_weights`, with lighter
-    cells corresponding to higher attention scores.
 
-    The diagram is saved with a filename that includes both the `layer_number`
-    and `head_number`.
-    """
-    # Create new image
-    image_size = GRID_SIZE * len(tokens) + PIXELS_PER_WORD
-    img = Image.new("RGBA", (image_size, image_size), "black")
-    draw = ImageDraw.Draw(img)
+    
 
-    # Draw each token onto the image
-    for i, token in enumerate(tokens):
-        # Draw token columns
-        token_image = Image.new("RGBA", (image_size, image_size), (0, 0, 0, 0))
-        token_draw = ImageDraw.Draw(token_image)
-        token_draw.text(
-            (image_size - PIXELS_PER_WORD, PIXELS_PER_WORD + i * GRID_SIZE),
-            token,
-            fill="white",
-            font=FONT
-        )
-        token_image = token_image.rotate(90)
-        img.paste(token_image, mask=token_image)
+    
 
-        # Draw token rows
-        _, _, width, _ = draw.textbbox((0, 0), token, font=FONT)
-        draw.text(
-            (PIXELS_PER_WORD - width, PIXELS_PER_WORD + i * GRID_SIZE),
-            token,
-            fill="white",
-            font=FONT
-        )
 
-    # Draw each word
-    for i in range(len(tokens)):
-        y = PIXELS_PER_WORD + i * GRID_SIZE
-        for j in range(len(tokens)):
-            x = PIXELS_PER_WORD + j * GRID_SIZE
-            color = get_color_for_attention_score(attention_weights[i][j])
-            draw.rectangle((x, y, x + GRID_SIZE, y + GRID_SIZE), fill=color)
 
-    # Save image
-    img.save(f"Attention_Layer{layer_number}_Head{head_number}.png")
 
 
 if __name__ == "__main__":
